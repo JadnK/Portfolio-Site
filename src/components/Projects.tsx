1529876b2a1d52d5fd646b46a6b4ecd84215975e
Jaden K. <96341029+Verpxnter@users.noreply.github.com>
2025-11-20 12:18:58 +0100

    Test: Only for test

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -40,7 +40,7 @@ export default function Projects() {
       github: 'https://github.com/jadnk/PiDash', 
     },
     {
-      title: 'FinGuard',
+      title: 'FinGua+rd',
       description:
         'Manage your finances effectively with FinGuard, a personal finance manager built with React and TypeScript.',
       image:
